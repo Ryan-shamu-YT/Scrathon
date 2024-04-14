e64efc788fb2584c6e3770e56ad564311f4e5fa2
Ryan-shamu-YT <83490776+Ryan-shamu-YT@users.noreply.github.com>
2024-04-14 17:24:34 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,44 +1,33 @@
-#If you know how to use scratchattach, just look at line 13 - 42
-
-import requests
+#If you know how to use scratchattach, just look at line 13 - 31
 import scratchattach as scratch3
+from ScrathonPayments import Scrathon
 
 session = scratch3.login("USERNAME_OF_ANY_ACCOUNT_(ACCOUNT_CAN_BE_NEW_SCRATCHER)", "PASSWORD")
 
-
 conn = session.connect_cloud("YOUR_PROJECT_ID")
+
+ScrathonPayments = Scrathon("USERNAME_OF_ACCOUNT_TO_RECEIVE_FUNDS")
 
 client = scratch3.CloudRequests(conn)
 
 @client.request
-def purchase(username, SellerName, Price):
-    request = requests.post("https://scrathon.justablock.online:1111/transaction", json={
-    "price": Price,
-    "buyer": username,
-	"seller": SellerName
-    })
+def purchase(username, Price):
+    Purchase = ScrathonPayments.purchase(Price, username)
 
-    if request.content == "You have been banned from selling items in your projects!":
+    if Purchase == "You have been banned from selling items in your projects!":
         return "Fail!"
         #In scratch, tell the user the transaction failed
-    elif request.content == "User does not have enough money to buy the item!":
+    elif Purchase == "User does not have enough money to buy the item!":
         return "Not enough coins!"
-            #In scratch, tell the user they do not have enough coins to make the purchase
-
-    elif request.content == "Transaction has been added to pending transactions, transaction will go through once the user has confirmed!":
+        #In scratch, tell the user they do not have enough coins to make the purchase
+    elif Purchase == "Transaction has been added to pending transactions, transaction will go through once the user has confirmed!":
         #In scratch, tell your users to look at the comments of the Scrathon project and find their verification comment in the project's comments, reply to it with 'yes' and the transaction will go through!
-        #if the user
-
         return "Success!"
 
 @client.request
-def purchasecheck(username, SellerName, Price): #Check if the player bought the item so you can give them the promised item (If you don't give the item to the player you will get banned from selling items)
-    request = requests.post("https://scrathon.justablock.online:1111/checkpurchase", json={
-    "price": Price,
-    "buyer": username,
-	"seller": SellerName
-    })
+def purchasecheck(username, Price): #Check if the player bought the item so you can give them the promised item (If you don't give the item to the player you will get banned from selling items)
+    PurchaseCheck = ScrathonPayments.purchasecheck(Price, username)
 
-    return request.content
+    return PurchaseCheck
 
 client.run()
